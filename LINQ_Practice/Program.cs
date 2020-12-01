d6be19a91ee6f9cc7291d07b3fcc39720bf9a04d
Cam Kellett <cannicam@gmail.com>
2020-12-01 11:03:36 -0600

    part 2 of exercise

---
+++ LINQ_Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string>();
+            gameList.Add("Halo");
+            gameList.Add("Kingdom Hearts");
+            gameList.Add("Skyrim");
+            gameList.Add("OverWatch");
+            gameList.Add("PUBG");
+            gameList.Add("Tekken 7");
+
+            var nameL = gameList.OrderBy(l => l.Length);
+            foreach(var i in nameL)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
